b02a8de13d8047d0c8ce45fe0c06a4cf49eeca9f
AnubhavSC <anubhavnath60@gmail.com>
2024-06-29 23:38:50 +0530

    git connected

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        kanit: 'Kanit',
+        Montserrat: 'Montserrat',
+        Nanum: 'Nanum Gothic', 
+        Gabarito: 'Gabarito',
+        Caveat: 'Caveat',
+        Rubik: ["Rubik Mono One"]
+      },
+    },
+  },
+  plugins: [],
+}
+
